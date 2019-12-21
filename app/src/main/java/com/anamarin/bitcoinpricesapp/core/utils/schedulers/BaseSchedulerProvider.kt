8ab7f9c906057230556b5c600a9b11ae8bd66149
Ana Marin <anamarinmartinez0904@gmail.com>
2019-12-21 06:53:45 +0100

    Improve chart interface

--- app/src/main/java/com/anamarin/bitcoinpricesapp/core/utils/schedulers/BaseSchedulerProvider.kt
+++
@@ -1,9 +0,0 @@
-package com.anamarin.bitcoinpricesapp.core.utils.schedulers
-
-import io.reactivex.Scheduler
-
-interface BaseSchedulerProvider {
-    fun io(): Scheduler
-    fun computation(): Scheduler
-    fun ui(): Scheduler
-}
